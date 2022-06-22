074a7ef1d62886e5dce7bfbef54c2cc81cfa1138
shivam <shivammaniya7469@gmail.com>
2022-06-21 22:34:11 -0400

    second commit

---
+++ client/Scripts/app.js
@@ -0,0 +1,30 @@
+//IIFE -- Immediately Invoked Function Expression
+"use strict";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+(function(){
+
+    function confirmDelete()
+    {
+      // confirm deletion
+      $("a.delete").on("click", function(event){
+        if(!confirm("Are you sure?"))
+        {
+          event.preventDefault();
+          location.href = '/books';
+        }       
+      });
+    }
+
+    function Start()
+    {
+        console.log("App Started");
+        
+        confirmDelete();  
+    }
+
+    window.addEventListener("load", Start);
+
+})();
+
+
---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "LocalURI": "mongodb://127.0.0.1/270127",
+  "RemoteURI": "mongodb+srv://shivam__09:lbSkIIggbasWU9r9@cluster0.cyhwz.mongodb.net/?retryWrites=true&w=majority",
+  "HostName": "LocalHost",
+  "Secret": "someSecret"
+};
